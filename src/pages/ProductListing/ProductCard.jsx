30bfc7b76969ecbb4d08c82bd1c1123f30d44006
Abhijeet Kumar <58811143+bkabhi@users.noreply.github.com>
2024-05-19 10:58:57 +0530

    Update ProductCard.jsx

--- src/pages/ProductListing/ProductCard.jsx
+++ src/pages/ProductListing/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addCartProducts } from '../../rudux/cart/action';
 
 const ProductCard = ({item}) => {
